2acbd7b43cd0cf7bf3aed1eb479fa302b91a5518
Donovan An <donovantaean@yahoo.com>
2017-02-28 09:54:48 -0800

    register route functional

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -4,11 +4,10 @@ using System.Threading.Tasks;
 using activityCenter.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace activityCenter.Controllers
-{
+namespace activityCenter.Controllers{
     public class UsersController : Controller{
-        private activityCenterContext _context;
 
+        private activityCenterContext _context;
         public UsersController(activityCenterContext context){
             _context = context;
         }
@@ -19,9 +18,23 @@ namespace activityCenter.Controllers
         // GET: /Home/
         [HttpGet]
         [Route("")]
-        public IActionResult Index()
-        {
+        public IActionResult Index(){
             return View();
         }
+
+        // POSt: /Home/
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register(){
+            System.Console.WriteLine("in register route!!");
+            System.Console.WriteLine("in register route!!");
+            System.Console.WriteLine("in register route!!");
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
     }
 }
