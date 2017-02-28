6c094f1613c499b12eda38970f80bfda000ad891
Donovan An <donovantaean@yahoo.com>
2017-02-28 11:32:38 -0800

    login/logout functional

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using activityCenter.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace activityCenter.Controllers{
@@ -20,6 +23,10 @@ namespace activityCenter.Controllers{
         [Route("")]
         public IActionResult Index(){
             ViewBag.Errors = new List<User>();
+            if(HttpContext.Session.GetString("LoginErrors") == null){
+                HttpContext.Session.SetString("LoginErrors", "");
+            }
+            ViewBag.LoginErrors = HttpContext.Session.GetString("LoginErrors");
             return View();
         }
 
@@ -29,14 +36,73 @@ namespace activityCenter.Controllers{
         public IActionResult Register(RegisterViewModel model){
             if(ModelState.IsValid){
                 System.Console.WriteLine("MODEL STATE IS VALID!!");
-                return RedirectToAction("Index");
+                User NewUser = new User{
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Password = model.Password,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                };
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                NewUser.Password = Hasher.HashPassword(NewUser, NewUser.Password);
+                _context.Add(NewUser);
+                _context.SaveChanges();
+                System.Console.WriteLine($"SUCCESSFULLY ADDED {NewUser.FirstName} {NewUser.LastName}!!");
+                
+
+                User CurrUser = _context.Users.Last();
+                HttpContext.Session.SetInt32("CurrUserId", CurrUser.UserId);
+                return RedirectToAction("Dashboard");
             } else {
                 ViewBag.Errors = ModelState.Values;
                 return View("Index");
             }
         }
 
+        // POST: /LogIn/
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(string email, string PwToCheck){
+                var user = _context.Users.SingleOrDefault( userr => userr.Email == email );
+                if( user != null && PwToCheck != null){
+                    var Hasher = new PasswordHasher<User>();
+                    if(0 != Hasher.VerifyHashedPassword(user, user.Password, PwToCheck)){
+                        HttpContext.Session.SetInt32("CurrUserId", user.UserId);
+                        return RedirectToAction("Dashboard");
+                    } else {
+                        HttpContext.Session.SetString("LoginErrors", "Invalid Name or Password");
+                        return RedirectToAction("Index");
+                    }
+                } else {
+                    HttpContext.Session.SetString("LoginErrors", "Invalid Name or Password");
+                    return RedirectToAction("Index");
+                }
+        }
 
+        // GET: /Dashboard/
+        [HttpGet]
+        [Route("Dashboard")]
+        public IActionResult Dashboard(){
+            System.Console.WriteLine("IN DASHBOARD METHOD!!");
+            ViewBag.CurrUser = _context.Users.SingleOrDefault( user => user.UserId == HttpContext.Session.GetInt32("CurrUserId") );
+            return View();
+        }
+
+
+
+
+
+
+
+
+        // GET: /Logout/
+        [HttpGet]
+        [Route("Logout")]
+        public IActionResult Logout(){
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
 
 
 
